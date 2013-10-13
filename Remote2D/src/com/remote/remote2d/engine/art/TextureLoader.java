6c4154c5a273dd5bc8dea39463a802516f701415
Flafla2 <awesomesauceface@aol.com>
2013-10-13 00:40:49 -0400

    Optomizations

--- Remote2D/src/com/remote/remote2d/engine/art/TextureLoader.java
+++ Remote2D/src/com/remote/remote2d/engine/art/TextureLoader.java
@@ -31,46 +31,47 @@ import com.remote.remote2d.engine.Remote2DException;
 
 public class TextureLoader {
 	private static final int BYTES_PER_PIXEL = 4;
+	
 	public static int loadTexture(BufferedImage image, boolean linearScaling, boolean repeat){
+		int[] pixels = new int[image.getWidth() * image.getHeight()];
+		image.getRGB(0, 0, image.getWidth(), image.getHeight(), pixels, 0, image.getWidth());
+
+		ByteBuffer buffer = BufferUtils.createByteBuffer(image.getWidth() * image.getHeight() * BYTES_PER_PIXEL); //4 for RGBA, 3 for RGB
+
+		for(int y = 0; y < image.getHeight(); y++){
+			for(int x = 0; x < image.getWidth(); x++){
+				int pixel = pixels[y * image.getWidth() + x];
+				buffer.put((byte) ((pixel >> 16) & 0xFF));     // Red component
+				buffer.put((byte) ((pixel >> 8) & 0xFF));      // Green component
+				buffer.put((byte) (pixel & 0xFF));               // Blue component
+				buffer.put((byte) ((pixel >> 24) & 0xFF));    // Alpha component. Only for RGBA
+			}
+		}
+
+		buffer.flip(); //FOR THE LOVE OF GOD DO NOT FORGET THIS
+
+		// You now have a ByteBuffer filled with the color data of each pixel.
+		// Now just create a texture ID and bind it. Then you can load it using 
+		// whatever OpenGL method you want, for example:
+
+		int textureID = glGenTextures(); //Generate texture ID
+		glBindTexture(GL_TEXTURE_2D, textureID); //Bind texture ID
+
+		//Setup wrap mode
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, repeat ? GL11.GL_REPEAT : GL12.GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, repeat ? GL11.GL_REPEAT : GL12.GL_CLAMP_TO_EDGE);
+
+		//Setup texture scaling filtering
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, linearScaling ? GL_LINEAR : GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, linearScaling ? GL_LINEAR : GL_NEAREST);
+
+		//Send texel data to OpenGL
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, image.getWidth(), image.getHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
 	      
-	      int[] pixels = new int[image.getWidth() * image.getHeight()];
-	        image.getRGB(0, 0, image.getWidth(), image.getHeight(), pixels, 0, image.getWidth());
-
-	        ByteBuffer buffer = BufferUtils.createByteBuffer(image.getWidth() * image.getHeight() * BYTES_PER_PIXEL); //4 for RGBA, 3 for RGB
-	        
-	        for(int y = 0; y < image.getHeight(); y++){
-	            for(int x = 0; x < image.getWidth(); x++){
-	                int pixel = pixels[y * image.getWidth() + x];
-	                buffer.put((byte) ((pixel >> 16) & 0xFF));     // Red component
-	                buffer.put((byte) ((pixel >> 8) & 0xFF));      // Green component
-	                buffer.put((byte) (pixel & 0xFF));               // Blue component
-	                buffer.put((byte) ((pixel >> 24) & 0xFF));    // Alpha component. Only for RGBA
-	            }
-	        }
-
-	        buffer.flip(); //FOR THE LOVE OF GOD DO NOT FORGET THIS
-
-	        // You now have a ByteBuffer filled with the color data of each pixel.
-	        // Now just create a texture ID and bind it. Then you can load it using 
-	        // whatever OpenGL method you want, for example:
-
-	      int textureID = glGenTextures(); //Generate texture ID
-	        glBindTexture(GL_TEXTURE_2D, textureID); //Bind texture ID
-	        
-	        //Setup wrap mode
-	        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, repeat ? GL11.GL_REPEAT : GL12.GL_CLAMP_TO_EDGE);
-	        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, repeat ? GL11.GL_REPEAT : GL12.GL_CLAMP_TO_EDGE);
-
-	        //Setup texture scaling filtering
-	        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, linearScaling ? GL_LINEAR : GL_NEAREST);
-	        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, linearScaling ? GL_LINEAR : GL_NEAREST);
-	        
-	        //Send texel data to OpenGL
-	        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, image.getWidth(), image.getHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
-	      
-	        //Return the texture ID so we can bind it later again
-	      return textureID;
-	   }
+		//Return the texture ID so we can bind it later again
+		buffer.clear();
+		return textureID;
+		}
 	   
 	   public static BufferedImage loadImage(String loc)
 	   {
